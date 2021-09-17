73ab6a3f038b4bce473a0efb8dbef23fa8cad656
Ryki Wong <rykiwong@RykideMac-mini.local>
2021-09-16 18:50:54 -0700

    Add airline route as polyline.

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,32 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([37.0902, -95.7129], 5);
+
+// Coordinates for each point to be used in the line.
+let line = [
+    [37.6213, -122.3790],
+    [30.1975, -97.6664],
+    [43.6777, -79.6248],
+    [40.6413, -73.7781]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    stroke: "blue",
+    dashArray: "10,10",
+    dashOffser: "0",
+    weight: 4,
+    lineOpacity: 0.5
+  }).addTo(map);
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'streets' tile layer to the map.
+streets.addTo(map);
