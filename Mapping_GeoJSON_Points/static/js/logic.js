043b5a43604a20ad0f05cb8438f2bdf177e4043b
Ryki Wong <rykiwong@RykideMac-mini.local>
2021-09-18 00:43:30 -0700

    bind popup on the map.

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -18,5 +18,11 @@ let airportData = "https://raw.githubusercontent.com/rykiprince/Mapping_Earthqua
 d3.json(airportData).then(function(data) {
   console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+  L.geoJson(data, {
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2><hr><h3>Airport name: " + feature.properties.name + "</h3>");
+    }
+  }).addTo(map);
+
 });
+
